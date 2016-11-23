a5af67989061043c8dc0d663c53e3ec1c291af60
Flavio Santos <flavio.santos@ext.ec.europa.eu>
2016-11-23 15:37:33 +0100

    EDELIVERY-1471 - fixing extracted certificate from the xml issue

--- dynamic-discovery-client/src/main/java/eu/europa/ec/dynamicdiscovery/core/security/X509KeySelector.java
+++ dynamic-discovery-client/src/main/java/eu/europa/ec/dynamicdiscovery/core/security/X509KeySelector.java
@@ -1,21 +1,3 @@
-/*
- * (C) Copyright 2016 Dynamic Discovery Client
- *
- * https://ec.europa.eu/cefdigital/code/projects/EDELIVERY/repos/dynamic-discovery-client/browse
- *
- * Licensed under the LGPL, Version 2.1 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     dynamic-discovery\License_LGPL-2.1.txt or https://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
 package eu.europa.ec.dynamicdiscovery.core.security;
 
 import javax.xml.crypto.*;
@@ -47,8 +29,10 @@ public class X509KeySelector extends KeySelector {
             Iterator xi = x509Data.getContent().iterator();
             while (xi.hasNext()) {
                 Object o = xi.next();
-                if (!(o instanceof X509Certificate))
+                if (!(o instanceof X509Certificate)) {
                     continue;
+                }
+                this.certificate = (X509Certificate) o;
                 final PublicKey key = ((X509Certificate) o).getPublicKey();
                 // Make sure the algorithm is compatible
                 // with the method.
--- dynamic-discovery-client/src/main/java/eu/europa/ec/dynamicdiscovery/core/security/XmldsigVerifier.java
+++ dynamic-discovery-client/src/main/java/eu/europa/ec/dynamicdiscovery/core/security/XmldsigVerifier.java
@@ -1,21 +1,3 @@
-/*
- * (C) Copyright 2016 Dynamic Discovery Client
- *
- * https://ec.europa.eu/cefdigital/code/projects/EDELIVERY/repos/dynamic-discovery-client/browse
- *
- * Licensed under the LGPL, Version 2.1 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     dynamic-discovery\License_LGPL-2.1.txt or https://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
 package eu.europa.ec.dynamicdiscovery.core.security;
 
 import eu.europa.ec.dynamicdiscovery.exception.SignatureException;
@@ -25,13 +7,12 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-import javax.xml.crypto.*;
-import javax.xml.crypto.dsig.*;
+import javax.xml.crypto.MarshalException;
+import javax.xml.crypto.dsig.Reference;
+import javax.xml.crypto.dsig.XMLSignature;
+import javax.xml.crypto.dsig.XMLSignatureException;
+import javax.xml.crypto.dsig.XMLSignatureFactory;
 import javax.xml.crypto.dsig.dom.DOMValidateContext;
-import javax.xml.crypto.dsig.keyinfo.KeyInfo;
-import javax.xml.crypto.dsig.keyinfo.X509Data;
-import java.security.Key;
-import java.security.PublicKey;
 import java.security.cert.X509Certificate;
 import java.util.Iterator;
 
@@ -81,45 +62,6 @@ public class XmldsigVerifier {
             return keySelector.getCertificate();
         } catch (XMLSignatureException | MarshalException e) {
             throw new SignatureException(e.getMessage(), e);
-        }
-    }
-
-    public class X509KeySelector extends KeySelector {
-        public KeySelectorResult select(KeyInfo keyInfo, KeySelector.Purpose purpose, AlgorithmMethod method,
-                                        XMLCryptoContext context) throws KeySelectorException {
-            Iterator ki = keyInfo.getContent().iterator();
-            while (ki.hasNext()) {
-                XMLStructure info = (XMLStructure) ki.next();
-                if (!(info instanceof X509Data))
-                    continue;
-                X509Data x509Data = (X509Data) info;
-                Iterator xi = x509Data.getContent().iterator();
-                while (xi.hasNext()) {
-                    Object o = xi.next();
-                    if (!(o instanceof X509Certificate))
-                        continue;
-                    final PublicKey key = ((X509Certificate) o).getPublicKey();
-                    // Make sure the algorithm is compatible
-                    // with the method.
-                    if (algEquals(method.getAlgorithm(), key.getAlgorithm())) {
-                        return new KeySelectorResult() {
-                            public Key getKey() {
-                                return key;
-                            }
-                        };
-                    }
-                }
-            }
-            throw new KeySelectorException("No key found!");
-        }
-
-        boolean algEquals(String algURI, String algName) {
-            if ((algName.equalsIgnoreCase("DSA") && algURI.equalsIgnoreCase(SignatureMethod.DSA_SHA1))
-                    || (algName.equalsIgnoreCase("RSA") && algURI.equalsIgnoreCase(SignatureMethod.RSA_SHA1))) {
-                return true;
-            } else {
-                return false;
-            }
         }
     }
 }
