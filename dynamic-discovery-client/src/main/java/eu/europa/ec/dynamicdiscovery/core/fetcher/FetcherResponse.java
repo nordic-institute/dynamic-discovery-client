141d60d02f8fb5e78ff8ac76ef0f0d41c6639332
Flavio Santos <flavio.santos@ext.ec.europa.eu>
2017-03-01 14:47:17 +0100

    no message

--- dynamic-discovery-client/src/main/java/eu/europa/ec/dynamicdiscovery/core/fetcher/FetcherResponse.java
+++ dynamic-discovery-client/src/main/java/eu/europa/ec/dynamicdiscovery/core/fetcher/FetcherResponse.java
@@ -7,7 +7,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     dynamic-discovery\License_LGPL-2.1.txt or https://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt
+ * dynamic-discovery\License_LGPL-2.1.txt or https://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
