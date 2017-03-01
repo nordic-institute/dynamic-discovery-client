86a87e6449e651db0a65f8ac79becb7faa4a869c
Flavio Santos <flavio.santos@ext.ec.europa.eu>
2017-03-01 14:50:59 +0100

    Merge branch 'Development' into bugfix/EDELIVERY-1969-removing-x-smp-namespace-from
    # Conflicts:
    #	dynamic-discovery-client/src/main/java/eu/europa/ec/dynamicdiscovery/core/fetcher/FetcherResponse.java

--- dynamic-discovery-client/src/main/java/eu/europa/ec/dynamicdiscovery/core/fetcher/FetcherResponse.java
+++ dynamic-discovery-client/src/main/java/eu/europa/ec/dynamicdiscovery/core/fetcher/FetcherResponse.java
@@ -26,18 +26,12 @@ import java.io.InputStream;
 public class FetcherResponse {
 
     private InputStream inputStream;
-    private String namespace;
 
-    public FetcherResponse(InputStream inputStream, String namespace) {
+    public FetcherResponse(InputStream inputStream) {
         this.inputStream = inputStream;
-        this.namespace = namespace;
     }
 
     public InputStream getInputStream() {
         return this.inputStream;
-    }
-
-    public String getNamespace() {
-        return this.namespace;
     }
 }
