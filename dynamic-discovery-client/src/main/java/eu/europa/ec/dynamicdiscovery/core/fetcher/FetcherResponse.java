6a015c519d778a5350092e5dd2025c9a9d96cb58
Flavio Santos <flavio.santos@ext.ec.europa.eu>
2017-03-01 14:49:50 +0100

    EDELIVERY-1969 - Removing X-SMP-Namespace from Dynamic Discovery Client

--- dynamic-discovery-client/src/main/java/eu/europa/ec/dynamicdiscovery/core/fetcher/FetcherResponse.java
+++ dynamic-discovery-client/src/main/java/eu/europa/ec/dynamicdiscovery/core/fetcher/FetcherResponse.java
@@ -24,14 +24,20 @@ package eu.europa.ec.dynamicdiscovery.core.fetcher;
 import java.io.InputStream;
 
 public class FetcherResponse {
-    
-    private InputStream inputStream;
 
-    public FetcherResponse(InputStream inputStream) {
-        this.inputStream = inputStream;;
+    private InputStream inputStream;
+    private String namespace;
+
+    public FetcherResponse(InputStream inputStream, String namespace) {
+        this.inputStream = inputStream;
+        this.namespace = namespace;
     }
 
     public InputStream getInputStream() {
         return this.inputStream;
+    }
+
+    public String getNamespace() {
+        return this.namespace;
     }
 }
