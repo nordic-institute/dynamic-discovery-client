6caaecb8dd993e607059d311be9a41a0f3bbd8d7
Joze RIHTARSIC <joze.RIHTARSIC@ext.ec.europa.eu>
2021-11-18 10:59:33 +0100

    Implement ebCorePartyID lookup

--- dynamic-discovery-client/src/test/java/eu/europa/ec/dynamicdiscovery/core/locator/impl/DefaultBDXRLocatorTest.java
+++ dynamic-discovery-client/src/test/java/eu/europa/ec/dynamicdiscovery/core/locator/impl/DefaultBDXRLocatorTest.java
@@ -18,19 +18,27 @@
 package eu.europa.ec.dynamicdiscovery.core.locator.impl;
 
 import eu.europa.ec.dynamicdiscovery.model.ParticipantIdentifier;
-import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.net.URI;
 
-import static org.mockito.Matchers.any;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.spy;
 
 /**
  * @author Flávio W. R. Santos
  */
+@RunWith(MockitoJUnitRunner.class)
 public class DefaultBDXRLocatorTest {
+
+    @Captor
+    ArgumentCaptor<String> naptrUrlCaptor;
 
     @Test
     public void testLookupNAPTR() throws Exception {
@@ -42,23 +50,64 @@ public class DefaultBDXRLocatorTest {
         URI uri = defaultBDXRLocator.lookup(participantIdentifier);
 
         //THEN
-        Assert.assertEquals("http://smp-mock-1.ehealth.eu:8888", uri.toString());
+        assertEquals("http://smp-mock-1.ehealth.eu:8888", uri.toString());
+        assertEquals("2CDN5ANIHSX2W6D2ZA5YSSGR2BXVLCGTLS6STIYM2CZYHB3L7GMA.country-state-qns.ehealth.acc.edelivery.tech.ec.europa.eu", naptrUrlCaptor.getValue());
     }
 
     @Test
-    public void testLookupNAPTR2() throws Exception {
+    public void testLookupNAPTRUpperCaseIdentifier() throws Exception {
         //GIVEN
         DefaultBDXRLocator defaultBDXRLocator = lookupNAPTR();
 
         //WHEN
-        URI uri = defaultBDXRLocator.lookup("urn:brazil:saopaulo", "country-state-qns");
+        URI uri = defaultBDXRLocator.lookup("urn:brazil:SAOPAULO", "country-state-qns");
 
         //THEN
-        Assert.assertEquals("http://smp-mock-1.ehealth.eu:8888", uri.toString());
+        assertEquals("http://smp-mock-1.ehealth.eu:8888", uri.toString());
+        assertEquals("2CDN5ANIHSX2W6D2ZA5YSSGR2BXVLCGTLS6STIYM2CZYHB3L7GMA.country-state-qns.ehealth.acc.edelivery.tech.ec.europa.eu", naptrUrlCaptor.getValue());
     }
 
     @Test
-    public void testLookupCNAME() throws Exception {
+    public void testLookupNAPTROasisPartyType() throws Exception {
+        //GIVEN
+        DefaultBDXRLocator defaultBDXRLocator = lookupNAPTR();
+
+        //WHEN
+        URI uri = defaultBDXRLocator.lookup("urn:brazil:saopaulo", "urn:oasis:names:tc:ebcore:partyid-type:unregistered");
+
+        //THEN
+        assertEquals("http://smp-mock-1.ehealth.eu:8888", uri.toString());
+        assertEquals("XN536BJVZUJJWWJZPQN5KAM6LFPK4ZZD2VL4AXQRELT5HTCJ6LEQ.ehealth.acc.edelivery.tech.ec.europa.eu", naptrUrlCaptor.getValue());
+    }
+
+    @Test
+    public void testLookupNAPTROasisPartyTypeNullScheme() throws Exception {
+        //GIVEN
+        DefaultBDXRLocator defaultBDXRLocator = lookupNAPTR();
+
+        //WHEN
+        URI uri = defaultBDXRLocator.lookup("urn:oasis:names:tc:ebcore:partyid-type:unregistered:urn:brazil:saopaulo", null);
+
+        //THEN
+        assertEquals("http://smp-mock-1.ehealth.eu:8888", uri.toString());
+        assertEquals("XN536BJVZUJJWWJZPQN5KAM6LFPK4ZZD2VL4AXQRELT5HTCJ6LEQ.ehealth.acc.edelivery.tech.ec.europa.eu", naptrUrlCaptor.getValue());
+    }
+
+    @Test
+    public void testLookupNAPTROasisPartyTypeEmptyScheme() throws Exception {
+        //GIVEN
+        DefaultBDXRLocator defaultBDXRLocator = lookupNAPTR();
+
+        //WHEN
+        URI uri = defaultBDXRLocator.lookup("urn:oasis:names:tc:ebcore:partyid-type:unregistered:urn:brazil:saopaulo", "");
+
+        //THEN
+        assertEquals("http://smp-mock-1.ehealth.eu:8888", uri.toString());
+        assertEquals("XN536BJVZUJJWWJZPQN5KAM6LFPK4ZZD2VL4AXQRELT5HTCJ6LEQ.ehealth.acc.edelivery.tech.ec.europa.eu", naptrUrlCaptor.getValue());
+    }
+
+    @Test
+    public void testLookupCNAMEPeppolPartyType() throws Exception {
         //GIVEN
         DefaultBDXRLocator defaultBDXRLocator = new DefaultBDXRLocator("ehealth.acc.edelivery.tech.ec.europa.eu");
         defaultBDXRLocator = spy(defaultBDXRLocator);
@@ -69,13 +118,58 @@ public class DefaultBDXRLocatorTest {
         URI uri = defaultBDXRLocator.lookup(participantIdentifier);
 
         //THEN
-        Assert.assertEquals("http://b-5cc29a6e1d849a3089cb7d8b192e55b7.country-state-qns.ehealth.acc.edelivery.tech.ec.europa.eu", uri.toString());
+        assertEquals("http://b-5cc29a6e1d849a3089cb7d8b192e55b7.country-state-qns.ehealth.acc.edelivery.tech.ec.europa.eu", uri.toString());
+    }
+
+    @Test
+    public void testLookupCNAMEOasisPartyType() throws Exception {
+        //GIVEN
+        DefaultBDXRLocator defaultBDXRLocator = new DefaultBDXRLocator("ehealth.acc.edelivery.tech.ec.europa.eu");
+        defaultBDXRLocator = spy(defaultBDXRLocator);
+        Mockito.doReturn(null).when(defaultBDXRLocator).naptrLookupFetcher(any(ParticipantIdentifier.class), any(String.class));
+        ParticipantIdentifier participantIdentifier = new ParticipantIdentifier("urn:brazil:saopaulo", "urn:oasis:names:tc:ebcore:partyid-type:unregistered");
+
+        //WHEN
+        URI uri = defaultBDXRLocator.lookup(participantIdentifier);
+
+        //THEN
+        assertEquals("http://b-761c04e661616234cd81659d456b0cf6.ehealth.acc.edelivery.tech.ec.europa.eu", uri.toString());
+    }
+
+    @Test
+    public void testLookupCNAMEOasisPartyTypeNullScheme() throws Exception {
+        //GIVEN
+        DefaultBDXRLocator defaultBDXRLocator = new DefaultBDXRLocator("ehealth.acc.edelivery.tech.ec.europa.eu");
+        defaultBDXRLocator = spy(defaultBDXRLocator);
+        Mockito.doReturn(null).when(defaultBDXRLocator).naptrLookupFetcher(any(ParticipantIdentifier.class), any(String.class));
+        ParticipantIdentifier participantIdentifier = new ParticipantIdentifier("urn:oasis:names:tc:ebcore:partyid-type:unregistered:urn:brazil:saopaulo", null);
+
+        //WHEN
+        URI uri = defaultBDXRLocator.lookup(participantIdentifier);
+
+        //THEN
+        assertEquals("http://b-761c04e661616234cd81659d456b0cf6.ehealth.acc.edelivery.tech.ec.europa.eu", uri.toString());
+    }
+
+    @Test
+    public void testLookupCNAMEOasisPartyTypeEmptyScheme() throws Exception {
+        //GIVEN
+        DefaultBDXRLocator defaultBDXRLocator = new DefaultBDXRLocator("ehealth.acc.edelivery.tech.ec.europa.eu");
+        defaultBDXRLocator = spy(defaultBDXRLocator);
+        Mockito.doReturn(null).when(defaultBDXRLocator).naptrLookupFetcher(any(ParticipantIdentifier.class), any(String.class));
+        ParticipantIdentifier participantIdentifier = new ParticipantIdentifier("urn:oasis:names:tc:ebcore:partyid-type:unregistered:urn:brazil:saopaulo", "");
+
+        //WHEN
+        URI uri = defaultBDXRLocator.lookup(participantIdentifier);
+
+        //THEN
+        assertEquals("http://b-761c04e661616234cd81659d456b0cf6.ehealth.acc.edelivery.tech.ec.europa.eu", uri.toString());
     }
 
     private DefaultBDXRLocator lookupNAPTR() throws Exception {
         DefaultBDXRLocator defaultBDXRLocator = new DefaultBDXRLocator("ehealth.acc.edelivery.tech.ec.europa.eu");
         defaultBDXRLocator = spy(defaultBDXRLocator);
-        Mockito.doReturn("http://smp-mock-1.ehealth.eu:8888").when(defaultBDXRLocator).naptrLookupFetcher(any(ParticipantIdentifier.class), any(String.class));
+        Mockito.doReturn("http://smp-mock-1.ehealth.eu:8888").when(defaultBDXRLocator).naptrLookupFetcher(any(ParticipantIdentifier.class), naptrUrlCaptor.capture());
 
         return defaultBDXRLocator;
     }
