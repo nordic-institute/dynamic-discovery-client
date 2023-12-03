e62d998c804f74bc4f93b4897ad2b842d56f21f8
RIHTARSIC Joze <joze.rihtarsic@ext.ec.europa.eu>
2023-12-03 10:06:01 +0100

    Add maven license plugin to handle file headers

--- dynamic-discovery-client/src/main/java/eu/europa/ec/dynamicdiscovery/core/fetcher/FetcherResponse.java
+++ dynamic-discovery-client/src/main/java/eu/europa/ec/dynamicdiscovery/core/fetcher/FetcherResponse.java
@@ -1,19 +1,21 @@
 /*
- * (C) Copyright 2016-2023 - European Commission | Dynamic Discovery Client
- *
- * https://ec.europa.eu/digital-building-blocks/code/projects/EDELIVERY/repos/dynamic-discovery-client/browse
- *
+ * #%L
+ * dynamic-discovery-cli
+ * %%
+ * Copyright (C) 2016 - 2023 European Commission | eDelivery | Dynamic Discovery Client
+ * %%
  * Licensed under the LGPL, Version 2.1 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- * dynamic-discovery\License_LGPL-2.1.txt or https://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt
- *
+ * 
+ * [PROJECT_HOME]\license\lgpl2-1\license.txt or https://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * #L%
  */
 package eu.europa.ec.dynamicdiscovery.core.fetcher;
 
