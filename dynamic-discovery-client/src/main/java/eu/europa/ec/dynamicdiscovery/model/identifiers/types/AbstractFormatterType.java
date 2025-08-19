34c6871af024f393c42e351b1b652c6390b353a3
Joze RIHTARSIC <joze.rihtarsic@ext.ec.europa.eu>
2025-08-19 09:45:10 +0200

    Improve identifier configuration

--- dynamic-discovery-client/src/main/java/eu/europa/ec/dynamicdiscovery/model/identifiers/types/AbstractFormatterType.java
+++ dynamic-discovery-client/src/main/java/eu/europa/ec/dynamicdiscovery/model/identifiers/types/AbstractFormatterType.java
@@ -36,6 +36,28 @@ public abstract class AbstractFormatterType implements  FormatterType {
 
     public static final String EBCORE_SEPARATOR = ":";
     public static final String OASIS_SMP_SEPARATOR = "::";
+    private static final String DEFAULT_SCHEME = "iso6523-actorid-upis";
+
+    /**
+     * Default constructor
+     */
+    @Deprecated
+    public AbstractFormatterType() {
+        // Default constructor
+    }
+    /**
+     * Constructor with builder
+     *
+     * @param builder Builder instance
+     */
+    protected AbstractFormatterType(AbstractFormatterBuilder<?> builder) {
+        this.isSchemeMandatory = builder.isSchemeMandatory;
+        this.wildcardEnabled = builder.wildcardEnabled;
+        this.schemeMaxLength = builder.schemeMaxLength;
+        this.valueMaxLength = builder.valueMaxLength;
+        this.valuePattern = builder.valuePattern;
+        this.schemePattern = builder.schemePattern;
+    }
 
     @Override
     public boolean isSchemeMandatory() {
@@ -94,5 +116,50 @@ public abstract class AbstractFormatterType implements  FormatterType {
     @Override
     public void setSchemeValidationPattern(Pattern schemePattern) {
         this.schemePattern = schemePattern;
+    }
+
+    public static abstract class AbstractFormatterBuilder<T extends AbstractFormatterType> {
+        boolean isSchemeMandatory = true;
+        boolean wildcardEnabled = true;
+        Integer schemeMaxLength;
+        Integer valueMaxLength;
+        Pattern valuePattern;
+        Pattern schemePattern;
+
+        public AbstractFormatterBuilder() {
+
+        }
+
+        public AbstractFormatterBuilder<T> schemeMandatory(boolean mandatory) {
+            this.isSchemeMandatory = mandatory;
+            return this;
+        }
+
+        public AbstractFormatterBuilder<T> wildcardEnabled(boolean enabled) {
+            this.wildcardEnabled = enabled;
+            return this;
+        }
+
+        public AbstractFormatterBuilder<T> schemeMaxLength(Integer length) {
+            this.schemeMaxLength = length;
+            return this;
+        }
+
+        public AbstractFormatterBuilder<T> valueMaxLength(Integer length) {
+            this.valueMaxLength = length;
+            return this;
+        }
+
+        public AbstractFormatterBuilder<T> valueValidationPattern(java.util.regex.Pattern pattern) {
+            this.valuePattern = pattern;
+            return this;
+        }
+
+        public AbstractFormatterBuilder<T> schemeValidationPattern(java.util.regex.Pattern pattern) {
+            this.schemePattern = pattern;
+            return this;
+        }
+
+        abstract public T build();
     }
 }
--- dynamic-discovery-client/src/main/java/eu/europa/ec/dynamicdiscovery/model/identifiers/types/PeppolPartyIdFormatterType.java
+++ dynamic-discovery-client/src/main/java/eu/europa/ec/dynamicdiscovery/model/identifiers/types/PeppolPartyIdFormatterType.java
@@ -7,9 +7,9 @@
  * Licensed under the LGPL, Version 2.1 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * [PROJECT_HOME]\license\lgpl2-1\license.txt or https://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,6 +18,8 @@
  * #L%
  */
 package eu.europa.ec.dynamicdiscovery.model.identifiers.types;
+
+import eu.europa.ec.dynamicdiscovery.enums.DNSLookupFormatType;
 
 import java.util.regex.Pattern;
 
@@ -28,23 +30,67 @@ import java.util.regex.Pattern;
  * @since 2.0
  */
 public class PeppolPartyIdFormatterType extends OasisSMPFormatterType {
-    static final Pattern PEPPOL_SCHEME_PATTERN = Pattern.compile("^[a-zA-Z0-9]+-[a-zA-Z0-9]+-[a-zA-Z0-9]+$");
+    static final Pattern PEPPOL_SCHEME_PATTERN = Pattern.compile("^(?i)(iso6523-actorid-upis)");
+    static final Pattern PEPPOL_MATCH_PATTERN = Pattern.compile("^(?i)\\s*(iso6523-actorid-upis::).*$");
+    static final Pattern PEPPOL_VALUE_PATTERN = Pattern.compile("^(?i)\\d{4}:.{1,135}$");
     static final int PEPPOL_SCHEME_MAX_SIZE = 25;
-    static final int PEPPOL_VALUE_MAX_SIZE = 50;
+    /**
+     *
+     * Policy for use of Identifiers 4.4.0 states that the maximum size of the value part of the identifier is 135 characters.
+     * e.g. 4 number  ICD + ':' + 130 characters for the value part of the identifier = 135.
+     */
+    static final int PEPPOL_VALUE_MAX_SIZE = 135;
 
 
+    /**
+     * @deprecated Use {@link OasisSMPFormatterType.Builder} to create an instance of this class.
+     */
+    @Deprecated
     public PeppolPartyIdFormatterType() {
-        setWildcardEnabled(true);
-        setValueMaxLength(PEPPOL_VALUE_MAX_SIZE);
-        setSchemeMandatory(false);
-        setSchemeMaxLength(PEPPOL_SCHEME_MAX_SIZE);
-        setSchemeValidationPattern(PEPPOL_SCHEME_PATTERN);
+        this(new PeppolPartyIdFormatterType.Builder());
+    }
+
+    protected PeppolPartyIdFormatterType(PeppolPartyIdFormatterType.Builder builder) {
+        super(builder);
     }
 
     @Override
     public String getInvalidSchemeMessage(String scheme, String identifier) {
-        return schemePattern!=null? super.getInvalidSchemeMessage(scheme, identifier):
+        return schemePattern != null ? super.getInvalidSchemeMessage(scheme, identifier) :
                 "The Scheme Identifier MUST take the form {domain}-{identifierArea}-{identifierType} such as for example 'busdox-actorid-upis'. It may only contain the following characters: [a-z0-9]+-[a-z0-9]+-[a-z0-9]+";
     }
 
+
+    /**
+     * Builder for PeppolPartyIdFormatterType formatter type creates the OasisSMPFormatterType with
+     * Peppol specific parameters (e.g. v4.4.0 Policy for use of Identifiers):
+     * <ul>
+     *     <li><b>Wildcard enabled:</b> true</li>
+     *     <li><b>Scheme mandatory:</b> false</li>
+     *     <li><b>Scheme max size:</b> 25 characters</li>
+     *     <li><b>Value max size:</b> 135 characters: The numeric identifier scheme is always 4 digits and the ":" separator added make it 4 + 1 + 130 = 135</li>
+     *     <li><b>Scheme pattern:</b> ^(?i)iso6523-actorid-upis</li>
+     *     <li><b>Match pattern:</b> ^(?i)iso6523-actorid-upis</li>
+     *     <li><b>Value pattern:</b> ^(?i)\\d{4}:.{1,130}$</li>
+     * </ul>
+     */
+    public static class Builder extends OasisSMPFormatterType.Builder {
+
+        public Builder() {
+            wildcardEnabled = true;
+            valueMaxLength = PEPPOL_VALUE_MAX_SIZE;
+            isSchemeMandatory = true;
+            schemeMaxLength = PEPPOL_SCHEME_MAX_SIZE;
+            schemePattern = PEPPOL_SCHEME_PATTERN;
+            matchPattern = PEPPOL_MATCH_PATTERN;
+            valuePattern = PEPPOL_VALUE_PATTERN;
+
+            dnsLookupFormatType = DNSLookupFormatType.SCHEMA_AFTER_HASH;
+        }
+
+        @Override
+        public PeppolPartyIdFormatterType build() {
+            return new PeppolPartyIdFormatterType(this);
+        }
+    }
 }
